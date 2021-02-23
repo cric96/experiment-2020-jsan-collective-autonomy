fe46a4bd91eb122a37de4965d8610481b9d098e4
Gianluca <gianluca.aguzzi@unibo.it>
2021-02-23 15:15:31 +0100

    add time limit

--- src/main/scala/it/unibo/alchemist/model/implementations/actions/IllAnimal.scala
+++ src/main/scala/it/unibo/alchemist/model/implementations/actions/IllAnimal.scala
@@ -1,16 +1,20 @@
 package it.unibo.alchemist.model.implementations.actions
 
 import it.unibo.alchemist.model.implementations.nodes.{Animal2D, SimpleNodeManager}
-import it.unibo.alchemist.model.interfaces.{Action, Context, Node, Reaction}
+import it.unibo.alchemist.model.interfaces.{Action, Context, Environment, Node, Reaction}
+import it.unibo.alchemist.scala.PimpMyAlchemist.time2Double
 
-case class IllAnimal[T](node : Animal2D[T]) extends AbstractAction[T](node) {
-  override def cloneAction(n: Node[T], r: Reaction[T]): Action[T] = IllAnimal(node) //todo fix this.. the clone must be done in the right place
+case class IllAnimal[T](env : Environment[T, _], node : Animal2D[T], timeLimit : Double) extends AbstractAction[T](node) {
+  def this(env : Environment[T, _], node : Animal2D[T]) {
+    this(env, node, Double.PositiveInfinity)
+  }
+  override def cloneAction(n: Node[T], r: Reaction[T]): Action[T] = IllAnimal(env, node, timeLimit) //todo fix this.. the clone must be done in the right place
   private val manger = new SimpleNodeManager[T](node)
   private var firstFire : Boolean = true
 
   override def execute(): Unit = if (firstFire) {
     firstFire = false
-  } else {
+  } else if (env.getSimulation.getTime < timeLimit) {
     manger.put("danger", true)
   }
 
